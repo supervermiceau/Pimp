25c24ba3287afcdf556377c8723fdba2ff1137e2
supervermiceau <supervermiceau@hotmail.com>
2016-02-26 22:43:07 +0000

    changes

--- TP/ihm/openGLCanvas.cpp
+++ TP/ihm/openGLCanvas.cpp
@@ -1,15 +1,16 @@
-#include "openGLCanvas.h"
-
+#include "openglcanvas.h"
+#include "mainframe.h"
+#include <wx/event.h>
+#include "triangle.h"
 //----------------------------------------------------------------------
-BEGIN_EVENT_TABLE(OpenGLCanvas, wxGLCanvas)
-    EVT_PAINT(OpenGLCanvas::OnPaint)
+BEGIN_EVENT_TABLE(OpenGLCanvas, wxGLCanvas)    
+	EVT_PAINT(OpenGLCanvas::OnPaint( wxPaintEvent& event);)
     EVT_SIZE(OpenGLCanvas::OnSize)
     EVT_ERASE_BACKGROUND(OpenGLCanvas::OnEraseBackground)
 END_EVENT_TABLE()
 //----------------------------------------------------------------------
 OpenGLCanvas::OpenGLCanvas(wxWindow *parent, wxWindowID id,const wxPoint& pos, const wxSize& size,long style, const wxString& name):wxGLCanvas(parent, id, pos, size, style, name)
 {
-
 }
 //----------------------------------------------------------------------
 OpenGLCanvas::~OpenGLCanvas(void)
@@ -18,19 +19,23 @@ OpenGLCanvas::~OpenGLCanvas(void)
 //----------------------------------------------------------------------
 void OpenGLCanvas::OnPaint( wxPaintEvent& event )
 {
-    wxPaintDC dc(this);
-    SetCurrent();
-    draw();
-    SwapBuffers();
+	wxPaintDC dc(this);
+	SetCurrent();
+	Draw();
+	SwapBuffers();
 }
 //----------------------------------------------------------------------
 void OpenGLCanvas::OnSize( wxSizeEvent& event )
-{
-    wxGLCanvas::OnSize(event);
+{	
+	wxGLCanvas::OnSize(event);
+
+	int w, h;
+	GetClientSize(&w, &h);
 }
 //----------------------------------------------------------------------
 void OpenGLCanvas::OnEraseBackground( wxEraseEvent& event )
 {
+	//ééé
 }
 //----------------------------------------------------------------------
 void OpenGLCanvas::Draw()
--- TP/ihm/openGLCanvas.h
+++ TP/ihm/openGLCanvas.h
@@ -2,19 +2,20 @@
 #define __OPENGLCANVAS_H__
 #include <wx/glcanvas.h>
 #include <wx/dcclient.h>
-#include <wx/event.h>
 #include "wx/wx.h"
 //----------------------------------------------------------------------
 class OpenGLCanvas : public wxGLCanvas 
 {
     public:
-        OpenGLCanvas(wxWindow *parent, wxWindowID id,const wxPoint& pos=wxDefaultPosition, const wxSize& size=wxDefaultSize,long style=0, const wxString& name=wxT("GLCANVAS"));
+		OpenGLCanvas(wxWindow *parent, wxWindowID id,const wxPoint& pos=wxDefaultPosition, const wxSize& size=wxDefaultSize,long style=0, const wxString& name=wxT("GLCANVAS"));
         ~OpenGLCanvas(void);
-        void Draw();
-    private:
-        DECLARE_EVENT_TABLE();
-			void OnPaint( wxPaintEvent& event );
-			void OnSize( wxSizeEvent& event );
-			void OnEraseBackground( wxEraseEvent& event );
 
+    private:
+    void OnPaint( wxPaintEvent& event);
+    void Onsize (wxSizeEvent& event);
+    void OnEraseBackground( wxEraseEvent& event);
+	DECLARE_EVENT_TABLE();
+    void Draw();
+};
+    //ééé
 #endif //__OPENGLCANVAS_H__
