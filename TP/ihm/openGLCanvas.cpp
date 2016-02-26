5f765abbb3353cf806f9b8e12c86af4b60b10922
supervermiceau <supervermiceau@hotmail.com>
2016-02-26 22:37:42 +0000

    oh yeh

--- TP/ihm/openGLCanvas.cpp
+++ TP/ihm/openGLCanvas.cpp
@@ -7,12 +7,6 @@ BEGIN_EVENT_TABLE(OpenGLCanvas, wxGLCanvas)
     EVT_PAINT(OpenGLCanvas::OnPaint)
     EVT_SIZE(OpenGLCanvas::OnSize)
     EVT_ERASE_BACKGROUND(OpenGLCanvas::OnEraseBackground)
-    EVT_MOTION(OpenGLCanvas::OnMouseMove)
-    EVT_LEFT_DOWN(OpenGLCanvas::OnLeftDown)
-    EVT_LEFT_UP(OpenGLCanvas::OnLeftUp)
-    EVT_RIGHT_DOWN(OpenGLCanvas::OnRightDown)
-    EVT_MENU(ID_SUPP,OpenGLCanvas::OnContextSupp)
-    EVT_MENU(ID_PROP,OpenGLCanvas::OnContextPptes)
 END_EVENT_TABLE()
 //----------------------------------------------------------------------
 OpenGLCanvas::OpenGLCanvas(wxWindow *parent, wxWindowID id,const wxPoint& pos, const wxSize& size,long style, const wxString& name):wxGLCanvas(parent, id, pos, size, style, name)
@@ -43,6 +37,5 @@ void OpenGLCanvas::OnEraseBackground( wxEraseEvent& event )
 //----------------------------------------------------------------------
 void OpenGLCanvas::Draw()
 {
-    
 }
 //----------------------------------------------------------------------
