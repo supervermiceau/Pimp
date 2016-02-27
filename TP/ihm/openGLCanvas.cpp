66373cf3f9f47a337f7715aeaa4830eabc4e161b
supervermiceau <supervermiceau@hotmail.com>
2016-02-27 18:16:09 +0000

    Dessin deubg

--- TP/ihm/openGLCanvas.cpp
+++ TP/ihm/openGLCanvas.cpp
@@ -74,9 +74,9 @@ void OpenGLCanvas::Draw()
 			}
   
         //gerer les 3 points
-        glVertex3f(main_frame->tab_tri[iCpt]->p1->x, maintmp->tab_tri[i].p1.y, 0.0f);
-        glVertex3f(main_frame->tab_tri[iCpt]->p2->x, maintmp->tab_tri[i].p2.y, 0.0f);
-        glVertex3f(main_frame->tab_tri[iCpt]->p3->x, maintmp->tab_tri[i].p3.y, 0.0f);
+        glVertex3f(main_frame->tab_tri[iCpt]->p1.x, maintmp->tab_tri[i].p1.y, 0.0f);
+        glVertex3f(main_frame->tab_tri[iCpt]->p2.x, maintmp->tab_tri[i].p2.y, 0.0f);
+        glVertex3f(main_frame->tab_tri[iCpt]->p3.x, maintmp->tab_tri[i].p3.y, 0.0f);
         glEnd();
         glBegin(GL_LINES);
         glColor3f(0.0,0.0,0.0);
