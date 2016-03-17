d6a387e781c071c5e0b773d85786f58278632f6f
supervermiceau <supervermiceau@hotmail.com>
2016-03-17 14:31:23 +0000

    Maj

--- TP/GL/Sources/Exo01/lecture.c
+++ TP/GL/Sources/Exo01/lecture.c
@@ -7,12 +7,16 @@ int main()
   FILE * f = fopen("fichiersource","r");
   char prenom[8], nom[8], classe[8];
 
-  fscanf(f,"%s\n%s\n%s\n\n",prenom,nom,classe);
-  fprintf(stderr,"%s\n%s\n%s\n\n",prenom,nom,classe);
-  fscanf(f,"%s\n%s\n%s\n\n",prenom,nom,classe);
-  fprintf(stderr,"%s\n%s\n%s\n\n",prenom,nom,classe);
-  fscanf(f,"%s\n%s\n%s\n\n",prenom,nom,classe);
-  fprintf(stderr,"%s\n%s\n%s\n\n",prenom,nom,classe);
 
+	while(feof(f) == 0)
+	{
+
+	  fscanf(f,"%s\n%s\n%s\n\n",prenom,nom,classe);
+	  fprintf(stderr,"%s\n%s\n%s\n\n",prenom,nom,classe);
+	  //~ fscanf(f,"%s\n%s\n%s\n\n",prenom,nom,classe);
+	  //~ fprintf(stderr,"%s\n%s\n%s\n\n",prenom,nom,classe);
+	  //~ fscanf(f,"%s\n%s\n%s\n\n",prenom,nom,classe);
+	  //~ fprintf(stderr,"%s\n%s\n%s\n\n",prenom,nom,classe);
+	}
   return 0;
 }
