0f0382c8806d57a4c12f0b2b47de776008f99074
supervermiceau <supervermiceau@hotmail.com>
2016-03-17 14:40:23 +0000

    maj

--- TP/GL/Sources/Exo02/redirect.c
+++ TP/GL/Sources/Exo02/redirect.c
@@ -8,12 +8,11 @@ int main()
   int valeurs[200];
   int indices[200];
   int i,j;
-
   /*Initialisation des valeurs à stocker*/
   for(i=0; i<=200; i++)
     valeurs[i] = i*i;
   
-  for(i=0; i<=200; i++)
+  for(j=0; j<=200; j++)
     indices[i] = 199-j;
     
   for(i=0; i<=200; i++)
