9489d272b60cd7243c6d09409bc0657b1f789025
supervermiceau <supervermiceau@hotmail.com>
2016-03-17 15:47:40 +0000

    Maj

--- TP/GL/Sources/Exo08/Temperature.c
+++ TP/GL/Sources/Exo08/Temperature.c
@@ -8,11 +8,11 @@ int main(int argc, char **argv)
     fprintf(stderr,"Cette fonction prend en entrée un entier (la température en degré Farenheit)\n");
     exit(1);
   }
-	int iTmp=atoi(argv[1]);
-	printf("%d jj\n",iTmp);
+	float iTmp=atof(argv[1]);
+	printf("%f jj\n",iTmp);
 
-	iTmp= (5/9)*(iTmp-32);
-printf("%d jj\n",iTmp);
+	iTmp= (5./9.)*(iTmp-32.);
+printf("%f jj\n",iTmp);
   fprintf(stderr,"%d degrés Farenheit correspondent à %d degrés Celsius\n",
 	  atoi(argv[1]),iTmp);
 
