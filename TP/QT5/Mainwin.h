6ca559f3cbdcd84c399dada815ecf3b4847e05ab
supervermiceau <supervermiceau@hotmail.com>
2016-03-18 14:49:56 +0000

    dd

--- TP/QT5/Mainwin.h
+++ TP/QT5/Mainwin.h
@@ -1,6 +1,8 @@
 #ifndef MAINWIN_H
 #define MAINWIN_H
 //----------------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
 #include "ui_mwin.h"
 //----------------------------------------------------------------------
 class Qonvertisseur : public QMainWindow, private Ui::MainWindow
